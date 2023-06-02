06ca4e8331e729ce9005ec74880c52f185ed6572
George Rosenberger <george@rosenberger.pro>
2023-06-02 10:28:42 +0200

    [FEATURE] Added Johnson2023 and Hijazi2020 datasets

---
+++ R/hijazi2020.R
@@ -0,0 +1,12 @@
+#' @title List of human kinase-substrate relationships measured by Hijazi et al., 2020.
+#' @description This data set contains a list of human kinase-substrate relationships measured by Hijazi et al., 2020.
+#' @format A \code{data.table} of 19287 K-S interactions.
+#' @references Hijazi, M., Smith, R., Rajeeve, V. et al. Reconstructing kinase network topologies from phosphoproteomics data reveals cancer-associated rewiring. Nat Biotechnol 38, 493–502 (2020). https://doi.org/10.1038/s41587-019-0391-9
+#' @source \url{https://doi.org/10.1038/s41587-019-0391-9}
+#' @author George Rosenberger
+#' @examples
+#' \dontrun{
+#' data(hijazi2020)
+#' summary(hijazi2020)
+#' }
+"hijazi2020"
---
+++ R/johnson2023.R
@@ -0,0 +1,12 @@
+#' @title List of human kinase-substrate relationships measured by Johnson et al., 2023.
+#' @description This data set contains a list of human kinase-substrate relationships measured by Johnson et al., 2023.
+#' @format A \code{data.table} of 26764293 K-S interactions.
+#' @references Johnson, J.L., Yaron, T.M., Huntsman, E.M. et al. An atlas of substrate specificities for the human serine/threonine kinome. Nature 613, 759–766 (2023).
+#' @source \url{https://doi.org/10.1038/s41586-022-05575-3}
+#' @author George Rosenberger
+#' @examples
+#' \dontrun{
+#' data(johnson2023)
+#' summary(johnson2023)
+#' }
+"johnson2023"
